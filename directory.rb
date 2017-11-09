b9e77a6b0b39a90558add42457ea06676127a5d3
Joe Pike <joseph.l.pike@gmail.com>
2017-11-09 17:13:46 +0000

    Added input_students metthod

--- directory.rb
+++ directory.rb
@@ -1,18 +1,20 @@
 student_count = 11
 
-students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
-]
+def input_students
+    puts "Please enter the name of the students"
+    puts "To finish, just hit the return twice"
+    
+    students = []
+    
+    name = gets.chomp
+    
+    while !name.empty? do
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        name = gets.chomp
+    end
+    students
+end
 
 def print_header
     # first we print the list of students
@@ -20,17 +22,18 @@ def print_header
     puts "-------------"
 end
 
-def print_names(names)
-    names.each do |name|
-        puts name
-    end
-end
-
 def print_footer
     # finally, we print the total number of students
     puts "Overall we have #{students.count} students"
 end
 
+def print(students)
+    students.each do |student|
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+end
+
+students = input_students
 print_header
-print_names(students)
+print(students)
 print_footer

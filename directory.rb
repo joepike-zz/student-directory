ea56d910a5e4fd262fae40ccf3c26e597097c673
Joe Pike <joseph.l.pike@gmail.com>
2017-11-12 14:33:28 +0000

    refactor 2, add save

--- directory.rb
+++ directory.rb
@@ -1,39 +1,81 @@
-student_count = 11
+@students = []
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+    
+def print_menu
+  puts "1. Input the students"
+  puts "2. Display the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "9"
+      exit
+    else
+      puts "I dont know what you meant, try again"
+  end
+end
 
 def input_students
-    puts "Please enter the name of the students"
-    puts "To finish, just hit the return twice"
-    
-    students = []
-    
+  puts "Enter the name of the student"
+  puts "Hit enter twice to exit"
+ 
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty?
+    # add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
     name = gets.chomp
-    
-    while !name.empty? do
-        students << {name: name, cohort: :november}
-        puts "Now we have #{students.count} students"
-        name = gets.chomp
-    end
-    students
+  end
 end
 
 def print_header
-    # first we print the list of students
-    puts "The students of Villians Academy"
-    puts "-------------"
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
+end
+
+def print_students_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
 
 def print_footer
-    # finally, we print the total number of students
-    puts "Overall we have #{students.count} students"
+  puts "Overall, we have #{@students.count} great students"
 end
 
-def print(students)
-    students.each do |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
-students = input_students
-print_header
-print(students)
-print_footer
+interactive_menu
+
